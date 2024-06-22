08267117a408ce64b739bed460ed27177d98be62
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-23 02:35:40 +0300

    created about.php

---
+++ models/about/about.php
@@ -0,0 +1,48 @@
+<!-- ======= Alt Services Section ======= -->
+<section id="alt-services" class="alt-services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row justify-content-around gy-4">
+          <div class="col-lg-6 img-bg" style="background-image: url(assets/img/alt-services.jpg);" data-aos="zoom-in" data-aos-delay="100"></div>
+
+          <div class="col-lg-5 d-flex flex-column justify-content-center">
+            <h3>ABOUT US</h3>
+            <p>We specialize in providing a comprehensive range of healthcare solutions, including rapid diagnostic kits, laboratory equipment, imaging and ICU solutions, orthopaedic implants and instruments, hospital furniture, dental and surgical equipment, point-of-care tests, digital health tests, and home tests.</p>
+
+            <div class="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="100">
+              <i class="bi bi-lightbulb flex-shrink-0"></i>
+              <div>
+                <h4><a href="" class="stretched-link">•	Innovation</a></h4>
+                <p>We constantly push boundaries to deliver groundbreaking solutions that redefine industry standards.</p>
+              </div>
+            </div><!-- End Icon Box -->
+
+            <div class="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="200">
+              <i class="bi bi-briefcase flex-shrink-0"></i>
+              <div>
+                <h4><a href="" class="stretched-link">•	Commitment</a></h4>
+                <p>Our commitment to excellence ensures that we consistently deliver high-quality solutions.</p>
+              </div>
+            </div><!-- End Icon Box -->
+
+            <div class="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="300">
+              <i class="bi bi-person-video3 flex-shrink-0"></i>
+              <div>
+                <h4><a href="" class="stretched-link">•	Integrity</a></h4>
+                <p>Integrity is the cornerstone of our business, ensuring trust and transparency in all we do.</p>
+              </div>
+            </div><!-- End Icon Box -->
+
+            <div class="icon-box d-flex position-relative" data-aos="fade-up" data-aos-delay="400">
+              <i class="bi bi-people flex-shrink-0"></i>
+              <div>
+                <h4><a href="" class="stretched-link">•	Teamwork</a></h4>
+                <p>Teamwork drives our success, fostering a culture where everyone's contributions are valued.</p>
+              </div>
+            </div><!-- End Icon Box -->
+
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End Alt Services Section -->
