98bda04812e18abf05282c23c58d5ebbcc9f3941
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-22 18:23:19 +0300

    created a carousel component

--- models/carousel/carousel.php
+++ models/carousel/carousel.php
@@ -4,8 +4,8 @@
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-lg-6 text-center">
-            <h2 data-aos="fade-down">Welcome to <span>UpConstruction</span></h2>
-            <p data-aos="fade-up">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <h2 data-aos="fade-down">Welcome to <span>Care Access Limited!</span></h2>
+            <p data-aos="fade-up">Established in 2022, Care Access has quickly become a trusted leader in the field of medical devices and supplies in Tanzania and within the region.</p>
             <a data-aos="fade-up" data-aos-delay="200" href="#get-started" class="btn-get-started">Get Started</a>
           </div>
         </div>
