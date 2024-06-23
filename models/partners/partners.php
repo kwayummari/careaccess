bfdb8a4d6e6c56a34029ecbb909c55ec355f6719
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-24 00:46:56 +0300

    created about.php

---
+++ models/partners/partners.php
@@ -0,0 +1,194 @@
+<!-- ======= Our Clients Section ======= -->
+<section id="clients">
+        <div class="container" data-aos="zoom-in">
+          <header class="section-header">
+            <h3>Our Clients</h3>
+          </header>
+
+          <div class="clients-slider swiper">
+            <div class="swiper-wrapper align-items-center">
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/hjfmri.svg"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  HJFMRI
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/tanesco.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  TANESCO
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/planInternational.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  PLAN INTERNATIONAL
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/university.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  UNIVERSITY OF DAR ES SALAAM
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/muhas.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  MUHAS
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/ngorongoro.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  MAMLAKA YA HIFADHI NGORONGORO
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/mwananyamala.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  MWANANYAMALA HOSPITAL
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/iris.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  IRIS
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/udom.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  UDOM
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/mantra.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  MANTRA TANZANIA
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/ICAP-logo.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  ICAP
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/undp.svg"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  UNDP
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/IHI.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  IHI
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/KCMUCo-LOGO-899x1024.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+                <h6>
+                  KCMUco
+                </h6>
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/logo-pathfinder-share-Twitter-color.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/mnhlogo.png"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+              </div>
+              <div class="swiper-slide">
+                <img
+                  src="assets/img/partners/OCEAN ROAD.jpeg"
+                  style="height: 100px;"
+                  class="img-fluid"
+                  alt=""
+                />
+              </div>
+              <!-- <div class="swiper-slide"><img src="assets/img/clients/client-7.png" class="img-fluid" alt=""></div>
+            <div class="swiper-slide"><img src="assets/img/clients/client-8.png" class="img-fluid" alt=""></div> -->
+            </div>
+            <div class="swiper-pagination"></div>
+          </div>
+        </div>
+      </section>
