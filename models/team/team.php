bce30ec72fb244f614118125f0d4188d22cf7a80
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-23 12:41:26 +0300

    created about.php

---
+++ models/team/team.php
@@ -0,0 +1,9 @@
+<section id="projects" class="projects">
+    <div class="container" data-aos="fade-up">
+        <div class="section-header">
+            <h2>MANAGEMENT TEAM</h2>
+            <p>Driven by a highly professional team of laboratory specialists, biomedical engineers, dental technicians, and clinicians, Care Access serves public and private health institutions, academic and research institutions, NGOs, and government entities.</p>
+        </div>
+        <img src="assets/img/team.png" />
+    </div>
+</section>
