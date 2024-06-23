4a8adcc414ac5d4986a38a0ce9b9af6dcc61e19c
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-24 00:28:56 +0300

    created about.php

---
+++ models/what/what.php
@@ -0,0 +1,106 @@
+
+    <!-- ======= Features Section ======= -->
+    <section id="features" class="features section-bg">
+      <div class="container" data-aos="fade-up">
+      <div class="section-header">
+            <h2>What We Do</h2>
+            <p>We aim to enable rapid diagnosis and improve patient prognosis by collaborating with leading brands that offer cutting.</p>
+        </div>
+        <ul class="nav nav-tabs row  g-1 d-flex">
+
+          <li class="nav-item col-3">
+            <a class="nav-link active show" data-bs-toggle="tab" data-bs-target="#tab-1">
+              <h4 class="h4">Distribution of medical devices and supplies</h4>
+            </a>
+          </li><!-- End tab nav item -->
+
+          <li class="nav-item col-3">
+            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#tab-2">
+              <h4>Importation of medical devices and supplies</h4>
+            </a><!-- End tab nav item -->
+
+          <li class="nav-item col-3">
+            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#tab-3">
+              <h4>Equipment installation and technical support</h4>
+            </a>
+          </li><!-- End tab nav item -->
+
+          <li class="nav-item col-3">
+            <a class="nav-link" data-bs-toggle="tab" data-bs-target="#tab-4">
+              <h4>Application training and consultation</h4>
+            </a>
+          </li><!-- End tab nav item -->
+
+        </ul>
+
+        <div class="tab-content">
+
+          <div class="tab-pane active show" id="tab-1">
+            <div class="row">
+              <div class="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
+                <h3>Distribution of medical devices and supplies</h3>
+                <ul>
+                  <li><i class="bi bi-check2-all"></i> <b>Reliable Distribution:</b> We ensure the timely distribution of medical devices and supplies, meeting the urgent needs of healthcare providers.</li>
+                  <li><i class="bi bi-check2-all"></i> <b>Comprehensive Network:</b> Our extensive distribution network guarantees that critical medical devices and supplies reach every corner swiftly and securely.</li>
+                  <li><i class="bi bi-check2-all"></i> <b>Quality Assurance:</b> We maintain the highest standards in the distribution of medical devices and supplies, ensuring they are delivered safely and effectively.</li>
+                </ul>
+              </div>
+              <div class="col-lg-6 order-1 order-lg-2 text-center" data-aos="fade-up" data-aos-delay="200">
+                <img src="assets/img/what/what1.jpg" alt="" class="img-fluid">
+              </div>
+            </div>
+          </div><!-- End tab content item -->
+
+          <div class="tab-pane" id="tab-2">
+            <div class="row">
+              <div class="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center">
+                <h3>Importation of medical devices and supplies</h3>
+                <ul>
+                  <li><i class="bi bi-check2-all"></i> <b>Global Sourcing:</b> We specialize in the importation of high-quality medical devices and supplies from trusted international manufacturers.</li>
+                  <li><i class="bi bi-check2-all"></i> <b>Regulatory Compliance:</b> Ensuring the importation of medical devices and supplies meets all global and local regulatory standards for safety and efficacy.</li>
+                  <li><i class="bi bi-check2-all"></i> <b>Reliable Supply Chain:</b> Our robust supply chain management ensures the timely and efficient importation of essential medical devices and supplies.</li>
+                  <li><i class="bi bi-check2-all"></i> <b>Quality Control:</b> We maintain stringent quality control measures in the importation process to guarantee the highest standards of medical devices and supplies.</li>
+                </ul>
+              </div>
+              <div class="col-lg-6 order-1 order-lg-2 text-center">
+                <img src="assets/img/what/what2.jpg" alt="" class="img-fluid">
+              </div>
+            </div>
+          </div><!-- End tab content item -->
+
+          <div class="tab-pane" id="tab-3">
+            <div class="row">
+              <div class="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center">
+                <h3>Equipment installation and technical support</h3>
+                <ul>
+                  <li><i class="bi bi-check2-all"></i> <b>Professional Setup:</b> Our expert team handles equipment installation with precision, ensuring optimal functionality from the start.</li>
+                  <li><i class="bi bi-check2-all"></i> <b>Ongoing Assistance:</b> We provide comprehensive technical support to ensure your equipment operates smoothly and efficiently at all times.</li>
+                  <li><i class="bi bi-check2-all"></i> <b>Seamless Integration:</b> Our installation services ensure that medical equipment is seamlessly integrated into your existing systems.</li>
+                </ul>
+              </div>
+              <div class="col-lg-6 order-1 order-lg-2 text-center">
+                <img src="assets/img/what/what3.jpg" alt="" class="img-fluid">
+              </div>
+            </div>
+          </div><!-- End tab content item -->
+
+          <div class="tab-pane" id="tab-4">
+            <div class="row">
+              <div class="col-lg-6 order-2 order-lg-1 mt-3 mt-lg-0 d-flex flex-column justify-content-center">
+                <h3>Application training and consultation</h3>
+                <ul>
+                  <li><i class="bi bi-check2-all"></i> <b>Hands-on Training:</b> We provide application training to ensure your team is proficient in using new equipment and technologies effectively.</li>
+                  <li><i class="bi bi-check2-all"></i> <b>Expert Consultation:</b> Our consultation services offer personalized guidance to optimize the use and performance of your medical devices.</li>
+                  <li><i class="bi bi-check2-all"></i> <b>Skill Enhancement:</b> Application training programs are designed to enhance your team's skills, maximizing the benefits of new equipment.</li>
+                </ul>
+              </div>
+              <div class="col-lg-6 order-1 order-lg-2 text-center">
+                <img src="assets/img/what/what4.jpg" alt="" class="img-fluid">
+              </div>
+            </div>
+          </div><!-- End tab content item -->
+
+        </div>
+
+      </div>
+    </section><!-- End Features Section -->
