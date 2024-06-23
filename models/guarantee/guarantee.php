6b8b67e734015ca4ba10dd344e5450ade51622a5
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-23 04:26:43 +0300

    created about.php

---
+++ models/guarantee/guarantee.php
@@ -0,0 +1,81 @@
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h2>OUR GUARANTEE</h2>
+          <p>We aim to enable rapid diagnosis and improve patient prognosis by collaborating with leading brands that offer cutting-edge laboratory, wellness at-home, and point-of-care solutions that are rapid, accessible, and affordable.</p>
+        </div>
+
+        <div class="row gy-4">
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+            <div class="service-item  position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-shopping-cart"></i>
+              </div>
+              <h3>•	Cutting-edge products</h3>
+              <p>Our commitment to cutting-edge products sets new standards in technology and functionality.</p>
+              <a href="service-details.php" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-cubes"></i>
+              </div>
+              <h3>•	Trusted brands</h3>
+              <p>Trusted brands built on years of consistent quality and customer satisfaction.</p>
+              <a href="service-details.php" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-bus"></i>
+              </div>
+              <h3>•	Prompt delivery and installation</h3>
+              <p>We ensure prompt delivery and installation, prioritizing efficiency without compromising quality.</p>
+              <a href="service-details.php" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-suitcase"></i>
+              </div>
+              <h3>•	Competitive pricing</h3>
+              <p>Offering competitive pricing without compromising on quality, ensuring affordability and value.</p>
+              <a href="service-details.php" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-globe"></i>
+              </div>
+              <h3>•	Technical expertise</h3>
+              <p>Our team possesses deep technical expertise, ensuring precise and innovative solutions.</p>
+              <a href="service-details.php" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+          <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
+            <div class="service-item position-relative">
+              <div class="icon">
+                <i class="fa-solid fa-volume-control-phone"></i>
+              </div>
+              <h3>•	Excellent customer service</h3>
+              <p>Providing excellent customer service with personalized attention and prompt solutions.</p>
+              <a href="service-details.php" class="readmore stretched-link">Learn more <i class="bi bi-arrow-right"></i></a>
+            </div>
+          </div><!-- End Service Item -->
+
+        </div>
+
+      </div>
+    </section><!-- End Services Section -->
---
+++ models/mission/mission.php
@@ -0,0 +1,33 @@
+<section id="featured-services">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4 box">
+              <i class="bi bi-briefcase"></i>
+              <h4 class="title"><a href="">Our Mission</a></h4>
+              <p class="description">
+              To provide cutting-edge medical devices and supplies with the best quality and most preferred healthcare solutions.
+              </p>
+            </div>
+
+            <div class="col-lg-4 box box-bg">
+              <i class="bi bi-binoculars"></i>
+              <h4 class="title"><a href="">Our Vision</a></h4>
+              <p class="description">
+              To enhance stakeholder value and facilitate healthcare providers in the region by delivering healthcare products, services, and end-to-end solutions in innovative and cost-effective ways.
+              </p>
+            </div>
+
+            <div class="col-lg-4 box">
+              <i class="bi bi-card-checklist"></i>
+              <h4 class="title"><a href="">Our Values</a></h4>
+              <p class="description">
+              •	Innovation <br />
+              •	Commitment <br />
+              •	Integrity <br />
+              •	Teamwork <br />
+              •	Excellence
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
