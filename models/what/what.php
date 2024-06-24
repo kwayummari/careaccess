6c9bc6acb65c70a49302e767986afaa5d486b6cc
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-24 19:21:24 +0300

    created about.php

--- models/what/what.php
+++ models/what/what.php
@@ -95,7 +95,7 @@
                 </ul>
               </div>
               <div class="col-lg-6 order-1 order-lg-2 text-center">
-                <img src="assets/img/what/what4.jpg" alt="" class="img-fluid">
+                <img src="assets/img/what/what5.jpg" alt="" class="img-fluid">
               </div>
             </div>
           </div><!-- End tab content item -->
