875f68bc4ed4cd2b2f51633b0311e00e109ebc65
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-24 20:09:27 +0300

    created about.php

--- models/products/icu.php
+++ models/products/icu.php
@@ -2,7 +2,7 @@
               <div class="portfolio-content h-100">
                 <img src="assets/img/icu/icu1.jpg" class="img-fluid" alt="">
                 <div class="portfolio-info">
-                  <h4>Forceps</h4>
+                  <h4></h4>
                   <p>Icu Equipment</p>
                   <a href="assets/img/icu/icu1.jpg" title="" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 </div>
@@ -13,7 +13,7 @@
               <div class="portfolio-content h-100">
                 <img src="assets/img/icu/icu2.jpg" class="img-fluid" alt="">
                 <div class="portfolio-info">
-                  <h4>Scissors</h4>
+                  <h4></h4>
                   <p>Icu Equipment</p>
                   <a href="assets/img/icu/icu2.jpg" title="" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 </div>
