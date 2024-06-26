aacf8592a4b92b67044287c74c697f60421160ae
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-26 03:25:33 +0300

    fixed

--- models/header/header.php
+++ models/header/header.php
@@ -1,12 +1,12 @@
 <?php include 'functions/setActiveMenu.php'; ?>
 
 <!-- ======= Header ======= -->
-<header id="header" class="header d-flex align-items-center">
+<header id="header" class="header d-flex align-items-center" style="background-color: white; color: black;">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
       <a href="index.php" class="logo d-flex align-items-center">
         <!-- Uncomment the line below if you also wish to use an image logo -->
-        <img src="assets/img/logo4.png" width="150px" height="100px" alt="">
+        <img src="assets/img/logo4.png" width="100px" height="50px" alt="">
         <!-- <h1>CARE ACCESS<span>.</span></h1> -->
       </a>
 
