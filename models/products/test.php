f9dd3028728a8a44b7d5e66ac9b1e66e92c3dad3
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-27 16:48:10 +0300

    fixed test.php

--- models/products/test.php
+++ models/products/test.php
@@ -1,65 +1,65 @@
-<div class="col-lg-4 col-md-6 portfolio-item filter-rapid">
+<div class="col-lg-4 col-md-6 portfolio-item filter-test">
               <div class="portfolio-content h-100">
-                <img src="assets/img/rapid/rapid1.jpg" class="img-fluid" alt="">
+                <img src="assets/img/test/test1.jpg" class="img-fluid" alt="">
                 <div class="portfolio-info">
                   <h4></h4>
-                  <p>Rapid diagnostic kits</p>
-                  <a href="assets/img/rapid/rapid1.jpg" title="" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                  <p>test diagnostic kits</p>
+                  <a href="assets/img/test/test1.jpg" title="" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 </div>
               </div>
             </div><!-- End Projects Item -->
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-rapid">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-test">
               <div class="portfolio-content h-100">
-                <img src="assets/img/rapid/rapid2.jpg" class="img-fluid" alt="">
+                <img src="assets/img/test/test2.jpg" class="img-fluid" alt="">
                 <div class="portfolio-info">
                   <h4></h4>
-                  <p>Rapid diagnostic kits</p>
-                  <a href="assets/img/rapid/rapid2.jpg" title="" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                  <p>test diagnostic kits</p>
+                  <a href="assets/img/test/test2.jpg" title="" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 </div>
               </div>
             </div><!-- End Projects Item -->
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-rapid">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-test">
               <div class="portfolio-content h-100">
-                <img src="assets/img/rapid/rapid3.jpg" class="img-fluid" alt="">
+                <img src="assets/img/test/test3.jpg" class="img-fluid" alt="">
                 <div class="portfolio-info">
                   <h4></h4>
-                  <p>Rapid diagnostic kits</p>
-                  <a href="assets/img/rapid/rapid3.jpg" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                  <p>test diagnostic kits</p>
+                  <a href="assets/img/test/test3.jpg" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 </div>
               </div>
             </div><!-- End Projects Item -->
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-rapid">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-test">
               <div class="portfolio-content h-100">
-                <img src="assets/img/rapid/rapid4.png" class="img-fluid" alt="">
+                <img src="assets/img/test/test4.png" class="img-fluid" alt="">
                 <div class="portfolio-info">
                   <h4></h4>
-                  <p>HAV Rapid Test</p>
-                  <a href="assets/img/rapid/rapid4.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                  <p>HAV test Test</p>
+                  <a href="assets/img/test/test4.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 </div>
               </div>
             </div><!-- End Projects Item -->
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-rapid">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-test">
               <div class="portfolio-content h-100">
-                <img src="assets/img/rapid/rapid5.png" class="img-fluid" alt="">
+                <img src="assets/img/test/test5.png" class="img-fluid" alt="">
                 <div class="portfolio-info">
                   <h4></h4>
                   <p>SARS-CoV-2</p>
-                  <a href="assets/img/rapid/rapid5.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                  <a href="assets/img/test/test5.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 </div>
               </div>
             </div><!-- End Projects Item -->
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-rapid">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-test">
               <div class="portfolio-content h-100">
-                <img src="assets/img/rapid/rapid6.png" class="img-fluid" alt="">
+                <img src="assets/img/test/test6.png" class="img-fluid" alt="">
                 <div class="portfolio-info">
                   <h4></h4>
                   <p>Multi-line Drug Cassette(Micro Saliva)</p>
-                  <a href="assets/img/rapid/rapid6.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                  <a href="assets/img/test/test6.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 </div>
               </div>
             </div><!-- End Projects Item -->
