06a54e8d1e3d6f568b6a0aa9d3b26ccff6eb40fc
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-27 17:10:22 +0300

    added test.php

--- models/products/health.php
+++ models/products/health.php
@@ -1,65 +1,32 @@
-<div class="col-lg-4 col-md-6 portfolio-item filter-test">
+<div class="col-lg-4 col-md-6 portfolio-item filter-health">
               <div class="portfolio-content h-100">
-                <img src="assets/img/test/test1.png" class="img-fluid" alt="">
+                <img src="assets/img/health/health1.png" class="img-fluid" alt="">
                 <div class="portfolio-info">
                   <h4></h4>
-                  <p>Blood Gas Analyzer</p>
-                  <a href="assets/img/test/test1.png" title="" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                  <p>Surely Smart</p>
+                  <a href="assets/img/health/health1.png" title="" data-gallery="portfolio-gallery-remodeling" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 </div>
               </div>
             </div><!-- End Projects Item -->
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-test">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-health">
               <div class="portfolio-content h-100">
-                <img src="assets/img/test/test2.png" class="img-fluid" alt="">
+                <img src="assets/img/health/health2.png" class="img-fluid" alt="">
                 <div class="portfolio-info">
                   <h4></h4>
-                  <p>Coagulation Analyzer</p>
-                  <a href="assets/img/test/test2.png" title="" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                  <p>Surely Smart Part</p>
+                  <a href="assets/img/health/health2.png" title="" data-gallery="portfolio-gallery-construction" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 </div>
               </div>
             </div><!-- End Projects Item -->
 
-            <div class="col-lg-4 col-md-6 portfolio-item filter-test">
+            <div class="col-lg-4 col-md-6 portfolio-item filter-health">
               <div class="portfolio-content h-100">
-                <img src="assets/img/test/test3.png" class="img-fluid" alt="">
+                <img src="assets/img/health/health3.png" class="img-fluid" alt="">
                 <div class="portfolio-info">
                   <h4></h4>
-                  <p>Automated Chemistry Analyzer</p>
-                  <a href="assets/img/test/test3.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
-                </div>
-              </div>
-            </div><!-- End Projects Item -->
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-test">
-              <div class="portfolio-content h-100">
-                <img src="assets/img/test/test4.png" class="img-fluid" alt="">
-                <div class="portfolio-info">
-                  <h4></h4>
-                  <p>Manual Microfluidics TRFIA Analyzer</p>
-                  <a href="assets/img/test/test4.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
-                </div>
-              </div>
-            </div><!-- End Projects Item -->
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-test">
-              <div class="portfolio-content h-100">
-                <img src="assets/img/test/test5.png" class="img-fluid" alt="">
-                <div class="portfolio-info">
-                  <h4></h4>
-                  <p>Semi-Automatic Microfluidics TRFIA Analyzer</p>
-                  <a href="assets/img/test/test5.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
-                </div>
-              </div>
-            </div><!-- End Projects Item -->
-
-            <div class="col-lg-4 col-md-6 portfolio-item filter-test">
-              <div class="portfolio-content h-100">
-                <img src="assets/img/test/test6.png" class="img-fluid" alt="">
-                <div class="portfolio-info">
-                  <h4></h4>
-                  <p>5-Part Automatic Veterinary Hematology Analyzer</p>
-                  <a href="assets/img/test/test6.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                  <p>Digital Ovulation Test</p>
+                  <a href="assets/img/health/health3.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
                 </div>
               </div>
             </div><!-- End Projects Item -->
