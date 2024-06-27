35a5a110d3c624485810d61f27115e57b3dd09c4
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-27 16:42:51 +0300

    fixed

--- models/products/orthopedic.php
+++ models/products/orthopedic.php
@@ -30,3 +30,36 @@
                 </div>
               </div>
             </div><!-- End Projects Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-orthopedic">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/orthopedic/orthopedic4.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4></h4>
+                  <p>DFN Distal Femur Intramedullary Nail </p>
+                  <a href="assets/img/orthopedic/orthopedic4.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                </div>
+              </div>
+            </div><!-- End Projects Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-orthopedic">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/orthopedic/orthopedic5.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4></h4>
+                  <p>Fixation Button </p>
+                  <a href="assets/img/orthopedic/orthopedic5.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                </div>
+              </div>
+            </div><!-- End Projects Item -->
+
+            <div class="col-lg-4 col-md-6 portfolio-item filter-orthopedic">
+              <div class="portfolio-content h-100">
+                <img src="assets/img/orthopedic/orthopedic6.png" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <h4></h4>
+                  <p>PEEK Interference Screw</p>
+                  <a href="assets/img/orthopedic/orthopedic6.png" title="" data-gallery="portfolio-gallery-repairs" class="glightbox preview-link"><i class="bi bi-zoom-in"></i></a>
+                </div>
+              </div>
+            </div><!-- End Projects Item -->
