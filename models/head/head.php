84558c81cfb044dd98aee347e01e2e5a418cfd49
Kwayu Mmari <mmaridenis@gmail.com>
2024-06-28 13:57:27 +0300

    added get random experiences

--- models/head/head.php
+++ models/head/head.php
@@ -3,8 +3,51 @@
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
   <title>CARE ACCESS</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
+  <meta
+      content="careaccess Tanzania,hospital Tanzania,clinics Tanzania, pharmacies Tanzania, healthcare Tanzania, health Tanzania,institute Tanzania,supplier Tanzania,distributor Tanzania,laboratory,Tanzania,devices,vendor,equipments,medical,medical supplies Tanzania,medical equipment Tanzania,medical supply company Tanzania,medical products Tanzania,hospital supplies Tanzania,medical equipment and supplies,health supplies Tanzania,medical products supply Tanzania,medical supply equipment Tanzania,health equipment Tanzania,best medical supply Tanzania,medical supplies & equipment Tanzania,health devices Tanzania,hospital medical supplies Tanzania,hospital medical equipment Tanzania,best medical supply company Tanzania,health care devices Tanzania,health medical supplies Tanzania,medical equipment medical supplies Tanzania,medical laboratory equipment Tanzania,medical supply products Tanzania,health care supplies companies Tanzania,medical equipment supply companyTanzania,medical laboratory supplies Tanzania,best health devices Tanzania,medical care supplies Tanzania,health medical equipment Tanzania,medical supplies supplier Tanzania,quality medical equipment Tanzania,hospital devices Tanzania,dental medical supplies Tanzania,best medical devices Tanzania,the medical supply Tanzania,medic medical supplies,medical medical supplies,medical supplies suppliers,pro medical equipment,hospital medical devices,best care medical supply,medical service equipment,dental medical equipment"
+      name="keywords"
+    />
+    <meta
+      name="description"
+      content="careaccess Tanzania is  medical device and equipments distributor in Tanzania founded in 2020  by a team of highly experienced, proficient, and adamant professionals to set an exceptional standard in the supply of medical and surgical goods to our customers and healthcare organizations."
+    />
+    <meta
+      name="robots"
+      content="index,follow,max-snippet:-1, max-video-preview:-1, max-image-preview:large"
+    />
+    <meta name="googlebot" content="index,follow" />
+    <meta name="googlebot-news" content="index,follow" />
+    <meta name="slurp" content="index,follow" />
+    <meta name="msnbot" content="index,follow" />
+    <link rel="canonical" href="https://careaccess.co.tz/" />
+    <meta property="og:type" content="website" />
+    <meta
+      property="og:title"
+      content="Best Medical Equipment Supplier In TANZANIA - careaccess"
+    />
+    <meta
+      property="og:description"
+      content="Careaccess is a medical equipment supplier across TANZANIA. We provide new technology to our clients, advancing health care in the markets we serve."
+    />
+    <meta property="og:url" content="https://careaccess.co.tz/" />
+    <meta property="og:site_name" content="careaccess Tanzania" />
+    <meta property="og:updated_time" content="2022-08-02T05:58:41+00:00" />
+    <meta property="video:duration" content="20" />
+    <meta property="ya:ovs:upload_date" content="2022-09-08" />
+    <meta property="ya:ovs:allow_embed" content="true" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta
+      name="twitter:title"
+      content="Best Medical Equipment Supplier In TANZANIA - careaccess"
+    />
+    <meta
+      name="twitter:description"
+      content="Careaccess is a medical equipment supplier across TANZANIA. We provide new technology to our clients, advancing health care in the markets we serve."
+    />
+    <meta name="twitter:label1" content="Written by" />
+    <meta name="twitter:data1" content="admin" />
+    <meta name="twitter:label2" content="Time to read" />
+    <meta name="twitter:data2" content="25 minutes" />
 
   <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
